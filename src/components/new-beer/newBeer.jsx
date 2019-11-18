9467e59defd3b6f6d2080957c7b6dd664e1a9c49
Juan Grajales <grajales.juanfe11@gmail.com>
2019-11-18 15:37:39 -0500

    add random beer feature

--- src/components/new-beer/newBeer.jsx
+++ src/components/new-beer/newBeer.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import axios from "axios" 
 
 export default class NewBeer extends Component {
-  render() {
+
+    state = {
+      name: "",
+    }
+  
+  
+    setBeer = (e) => {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+
+    sendBeerToApi = (e) => {
+      e.preventDefault()
+      axios.post("https://ih-beers-api2.herokuapp.com/beers/new", {name: this.state.name})
+      .then((result) => {
+        console.log(result)
+      })
+    }
+  
+    render() {
     return (
       <div>
         NewBeer
+        <form onSubmit={this.sendBeerToApi}>
+          <input type="text" name="name" onChange={this.setBeer} />
+          <input type="submit" value="add a beer" />
+        </form>
       </div>
     )
   }
