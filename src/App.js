7aab85a6f3fa1d48963df85c494290ccaf8367d3
Juan Grajales <grajales.juanfe11@gmail.com>
2019-11-18 12:50:05 -0500

    add react-router-dom and axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios" 
+import {Switch, Link, Route} from "react-router-dom"
 
 class App extends Component {
+
+componentDidMount() {
+  this.getAllBeers();
+}
+
+getAllBeers = () => {
+  axios.get("https://ih-beers-api2.herokuapp.com/beers").then(allTheBeers => {
+    console.log(allTheBeers);
+  })
+}
+
+getRandomBeer = () => {
+  axios.get("https://ih-beers-api2.herokuapp.com/beers/random").then(randomBeer => {
+    console.log(randomBeer);
+  })
+}
+
   render() {
     return (
       <div className="App">
